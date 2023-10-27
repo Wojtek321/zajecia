0cb2b4ab672e2ba715cb6e519bd46aab04ef8adb
Wojciech Domański <wojtekdxd@gmail.com>
2023-10-27 23:37:02 +0200

    wav

--- psio/zajecia2/main.py
+++ psio/zajecia2/main.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import square, sawtooth
-from IPython.display import Audio
 from time import sleep
+import sounddevice as sd
+import wave
+import struct
 
 
 # # Zadanie 1
-# a)
+# # a)
 # x = np.zeros((1, 41))
 # x[0][0] = 1
 # x[0][40] = 1
@@ -22,31 +24,20 @@ from time import sleep
 # # b)
 # N = 200
 # x = np.linspace(0, 2*5*np.pi, N)
-# y1 = np.sin(x)
-# y2 = sawtooth(x)
-# y3 = square(x)
+# Y = [np.sin(x), sawtooth(x), square(x)]
 #
-# plt.subplot(3, 1, 1)
-# plt.plot(x, y1)
-# plt.xlabel('Kat [rad]')
-# plt.ylabel('Amplituda')
-#
-# plt.subplot(3, 1, 2)
-# plt.plot(x, y2)
-# plt.xlabel('Kat [rad]')
-# plt.ylabel('Amplituda')
-#
-# plt.subplot(3, 1, 3)
-# plt.plot(x, y3)
-# plt.xlabel('Kat [rad]')
-# plt.ylabel('Amplituda')
+# for i in range(0, 3):
+#     plt.subplot(3, 1, i+1)
+#     plt.plot(x, Y[i])
+#     plt.xlabel('Kat [rad]')
+#     plt.ylabel('Amplituda')
+# plt.tight_layout()
 # plt.show()
 #
 #
 # #c)
 # n = 200
 # y = np.random.normal(loc=0, scale=0.5, size=n)
-#
 # plt.plot(y)
 # plt.xlabel("Numer probki")
 # plt.ylabel('Wartosc chwilowa')
@@ -55,13 +46,13 @@ from time import sleep
 
 
 # # Zadanie 2
-A = [1, 2, 5]
-F = [1000, 2000, 5000]
-phi = [0, -np.pi/2, np.pi/2]
-fs = 44100
-dur = 1
-t = np.arange(start=0, stop=dur, step=1/fs)
-n = np.arange(0, len(t))
+# A = [1, 2, 5]
+# F = [1000, 2000, 5000]
+# phi = [0, -np.pi/2, np.pi/2]
+# fs = 44100
+# dur = 2
+# t = np.arange(start=0, stop=dur, step=1/fs)
+# n = np.arange(0, len(t))
 #
 # i=1
 # for a in A:
@@ -74,8 +65,8 @@ n = np.arange(0, len(t))
 #     plt.grid(True)
 #     plt.axis((0, 0.003, -5, 5))
 #     i += 1
-#     # Audio(y, rate=44100)
-#     # sleep(2)
+#     sd.play(y, fs)
+#     sd.wait()
 # plt.tight_layout()
 # plt.show()
 #
@@ -90,6 +81,8 @@ n = np.arange(0, len(t))
 #     plt.grid(True)
 #     plt.axis((0, 0.003, -1, 1))
 #     i += 1
+#     sd.play(y, fs)
+#     sd.wait()
 # plt.tight_layout()
 # plt.show()
 #
@@ -111,14 +104,16 @@ n = np.arange(0, len(t))
 #     plt.grid(True)
 #     plt.axis((0, 0.003, -1, 1))
 #     i += 1
+#     sd.play(y, fs)
+#     sd.wait()
 # plt.tight_layout()
 # plt.show()
 
 
-# Zadanie 3
+# # Zadanie 3
 # f = 1000
 # FS = [8000, 2000, 1100]
-# dur = 0.007
+# dur = 0.2
 #
 # i=1
 # for fs in FS:
@@ -134,6 +129,57 @@ n = np.arange(0, len(t))
 #     plt.ylabel("Amplituda")
 #     plt.grid(True)
 #     i += 1
+#     sd.play(y, fs)
+#     sd.wait()
 # plt.tight_layout()
 # plt.show()
 
+
+# Zadanie 4
+# a)
+with wave.open("odliczanie.wav", 'r') as file:
+    fs = file.getframerate()
+    n_frames = file.getnframes()
+    data = file.readframes(n_frames)
+    sample_width = file.getsampwidth()
+    samples = struct.unpack(f"{n_frames}h", data)
+
+max_amplitude = np.max(np.abs(samples))
+samples = samples / max_amplitude
+reversed_samples = samples[::-1]
+
+time = np.arange(0, n_frames)/fs
+plt.figure(figsize=(6, 8))
+plt.subplot(4, 1, 1)
+plt.plot(time, samples)
+plt.title("Mowa")
+plt.xlabel("Czas [s]")
+plt.ylabel("Amplituda")
+
+plt.subplot(4, 1, 2)
+plt.plot(time, reversed_samples)
+plt.title("Mowa odwrocona w czasie")
+plt.xlabel("Czas [s]")
+plt.ylabel("Amplituda")
+
+# sd.play(reversed_samples, fs)
+# sd.wait()
+
+
+#b)
+y = np.random.normal(loc=0, scale=1, size=n_frames)
+plt.subplot(4, 1, 3)
+plt.plot(time, y)
+plt.title(f'Szum Gaussowski, μ={round(np.mean(y), 4)}, σ^2={round(np.std(y), 4)}')
+plt.ylabel("Amplituda")
+plt.xlabel("Czas [s]")
+
+moc_sygnalu = np.sum(np.power(samples, 2))
+moc_szumu = np.sum(np.power(y, 2))
+
+SNR = 10 * np.log10(moc_sygnalu / moc_szumu)
+print(SNR)
+
+
+# plt.tight_layout()
+# plt.show()
