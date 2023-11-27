8983348204ba70b954419b8ed69ee1d4308990d8
Wojtek321 <wojtekdxd@gmail.com>
2023-11-27 17:32:36 +0100

    sdfsadf

--- asd/zajecia3/zad2/main.cpp
+++ asd/zajecia3/zad2/main.cpp
@@ -1,31 +1,124 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+void Plecak(int *p, int *w, int n, int W);
 
 int main()
 {
-    ifstream plik("C:\\Users\\wojte\\Desktop\\zajecia\\asd\\zajecia3\\zad2\\In0302.txt");
+    ifstream plik("C:\\Users\\Wojtek\\Desktop\\zajecia\\asd\\zajecia3\\zad2\\In0302.txt");
     int n, W;
     plik>>n>>W;
 
-    int** tab = new int*[n];
-    for(int i = 0; i < n; i++)
-        tab[i] = new int[2];
+    int* w = new int[n];
+    int* p = new int[n];
 
     for(int i=0; i<n; i++)
     {
-        plik>>tab[i][0]>>tab[i][1];
+        plik>>p[i]>>w[i];
     }
     plik.close();
 
+    Plecak(p, w, n, W);
+}
 
-    for(int i=0; i<n; i++)
+void Plecak(int *p, int *w, int n, int W)
+{
+    int** S1 = new int*[W+1];
+    int** S2 = new int*[W+1];
+    for(int i = 0; i < W+1; i++)
     {
-        cout<<tab[i][0]<<" "<<tab[i][1]<<endl;
+        S1[i] = new int[n+1] {};
+        S2[i] = new int[n+1] {};
     }
 
 
-    int S[5][5] = {};
-    
+    for(int i=0; i<W+1; i++)
+    {
+        for(int j=0; j<n+1; j++)
+        {
+            if(i==0 || j==0)
+            {
+                S1[i][j] = S2[i][j] = 0;
+            }
+            else if(i-w[j-1] >= 0)
+            {
+                int pom = S1[i-w[j-1]][j-1] + p[j-1];
+
+                if(pom < S1[i][j-1])
+                {
+                    S1[i][j] = S1[i][j-1];
+                }
+                else
+                {
+                    S1[i][j] = pom;
+                    S2[i][j] = 1;
+                }
+            }
+            else
+            {
+                S1[i][j] = S1[i][j-1];
+            }
+        }
+    }
+
+
+//    for(int i=0; i<W+1; i++)
+//    {
+//        for(int j=0; j<n+1; j++)
+//            cout<<S1[i][j]<<" ";
+//        cout<<endl;
+//    }
+//    cout<<endl;
+//    for(int i=0; i<W+1; i++)
+//    {
+//        for(int j=0; j<n+1; j++)
+//            cout<<S2[i][j]<<" ";
+//        cout<<endl;
+//    }
+
+
+    std::vector<int> kombinacje;
+    int i;
+    int j;
+    int maks = S1[W][n];
+    ofstream out("C:\\Users\\Wojtek\\Desktop\\zajecia\\asd\\zajecia3\\zad2\\Out0302.txt");
+
+    for(int a=n; a>0; a--)
+    {
+        j = a;
+        i = W;
+
+        if(S1[i][j] == maks)
+        {
+            kombinacje.push_back(j);
+            i -= w[j-1];
+            while(j>0)
+            {
+                if(S2[i][j])
+                {
+                    kombinacje.push_back(j);
+                    break;
+                }
+                j--;
+            }
+        }
+
+        if(kombinacje.size() != 0)
+        {
+            for (int b = kombinacje.size() - 1; b >= 0; b--)
+            {
+                out << kombinacje[b] << " ";
+            }
+            kombinacje.clear();
+            out << endl;
+        }
+
+
+    }
+
+    delete []S1;
+    delete []S2;
+    out.close();
 }
