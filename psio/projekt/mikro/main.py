728efe6a236e4598e152238327e1ad91b091137c
Wojciech Domański <wojtekdxd@gmail.com>
2023-12-16 23:01:32 +0100

    dsf

--- psio/projekt/mikro/main.py
+++ psio/projekt/mikro/main.py
@@ -12,6 +12,8 @@ from pprint import pprint
 from threading import Thread
 import tkinter as tk
 import random
+import pyaudio
+from PIL import Image, ImageTk
 
 
 
@@ -19,7 +21,7 @@ TIME = 2
 ANIMATION_INTERVAL = 30
 FS = 44100
 HEAD_WIDTH = 0.15
-AMOUNT_OF_SPEAKERS = 7
+AMOUNT_OF_SPEAKERS = 9
 DOWNSAMPLE = 1
 
 ARROW_LENGTH = 200
@@ -32,7 +34,11 @@ N = TIME*FS
 
 
 devices = sd.query_devices()
-mic_name = devices[1]['name']
+# print(devices)
+mic1 = devices[1]['name']
+mic2 = devices[2]['name']
+# print(mic1)
+# print(mic2)
 
 n = np.arange(0, TIME, 1/FS)
 plotdata = np.zeros((N, 1))
@@ -47,8 +53,8 @@ q = queue.Queue()
 max_offset = HEAD_WIDTH/340
 interval = 2*max_offset/AMOUNT_OF_SPEAKERS
 
-print("max offset: ", max_offset)
-print("interval: ", interval)
+# print("max offset: ", max_offset)
+# print("interval: ", interval)
 
 dict_of_intervals = {}
 for i in range(-(AMOUNT_OF_SPEAKERS//2), (AMOUNT_OF_SPEAKERS//2)+1):
@@ -86,26 +92,33 @@ def plot_init():
     return line,
 
 
-file_path = "039.wav"
-_, muzyka = wavfile.read(file_path)
-muzyka = np.array(muzyka)
-muzyka = np.sum(muzyka, axis=1)
-max_amplitude = np.max(np.abs(muzyka))
-muzyka = muzyka/ max_amplitude
-muzyka = muzyka[:1136]
+# file_path = "039.wav"
+# _, muzyka = wavfile.read(file_path)
+# muzyka = np.array(muzyka)
+# muzyka = np.sum(muzyka, axis=1)
+# max_amplitude = np.max(np.abs(muzyka))
+# muzyka = muzyka/ max_amplitude
+# muzyka = muzyka[:1136]
+#
+# zera = np.zeros(11)
+# przesuniety = np.concatenate((zera, muzyka))
 
-zera = np.zeros(11)
-przesuniety = np.concatenate((zera, muzyka))
 
-
-def audio_callback(indata, frames, time, status):
+def audio_callback1(indata, frames, time, status):
     data = np.sum(indata, axis=1)/2
-    i = random.randint(0, 10)
-    zera = np.zeros(i)
-    przesuniety = np.concatenate((zera, muzyka))
-    speaker_no = categoriser(muzyka, przesuniety)[0]
+    # i = random.randint(0, 10)
+    # zera = np.zeros(i)
+    # przesuniety = np.concatenate((zera, muzyka))
+    # speaker_no = categoriser(muzyka, przesuniety)[0]
+    #
+    # update_arrow(dictionary_arrow[speaker_no][0], dictionary_arrow[speaker_no][1])
+    # print(len(data))
+    print("1")
+    q.put(data[::DOWNSAMPLE])
 
-    update_arrow(dictionary_arrow[speaker_no][0], dictionary_arrow[speaker_no][1])
+def audio_callback2(indata, frames, time, status):
+    data = np.sum(indata, axis=1)/2
+    print("2")
     q.put(data[::DOWNSAMPLE])
 
 
@@ -158,8 +171,8 @@ def gcc_phat(sig, refsig, fs=44100, max_tau=None, interp=16):
 
 
 def coordinates_of_speakers(angle):
-    x = WIDTH / 2 - np.cos(np.radians(angle)) * 250 - 3
-    y = HEIGHT - np.sin(np.radians(angle)) * 250 - 85
+    x = WIDTH / 2 - np.cos(np.radians(angle)) * 250 - 27
+    y = HEIGHT - np.sin(np.radians(angle)) * 250 - 110
     return x,y
 
 
@@ -182,16 +195,25 @@ root.resizable(width=False, height=False)
 root.configure(background='#00c3e3')
 root.title("yś")
 root.geometry('600x400')
-line_canvas = tk.Canvas(root, width=620, height=400, background='#00c3e3')
+line_canvas = tk.Canvas(root, highlightthickness=0, width=620, height=400, background='#00c3e3')
 line_canvas.place(x=0, y=0)
 
+angle_of_rotate = 0
 for i in range(0,AMOUNT_OF_SPEAKERS):
-    speakers_canvas = tk.Canvas(root, width=WIDTH_SPEAKER, height=HEIGHT_SPEAKER)
-    speakers_canvas.configure(bg='black')
-    angle = i*ANGLE
-    x,y = coordinates_of_speakers(angle)
-    speakers_canvas.place(x=x-15,y=y+15)
+    speakers_canvas = tk.Canvas(root,highlightthickness=0, width=70, height=70,background='#00c3e3')
+    speakers_canvas.pack()
+    original_image = Image.open("speaker.png")
+    resized_image = original_image.resize((90, 90))
 
+    rotated_image = resized_image.rotate(angle_of_rotate)
+    image = ImageTk.PhotoImage(rotated_image)
+    speakers_canvas.image = image
+    speakers_canvas.create_image(-3,-2,image=image,anchor='nw')
+    angle = i * ANGLE
+    x, y = coordinates_of_speakers(angle)
+    speakers_canvas.place(x=x - 15, y=y + 15)
+
+    angle_of_rotate -= ANGLE
 
 
 
@@ -215,36 +237,58 @@ def update_arrow(x, y):
     line_canvas.coords(arrow, 300, 334, x, y)
 
 
-# def update(i):
-#     if i<=7:
-#         x = dictionary_arrow[i][0]
-#         y = dictionary_arrow[i][1]
-#         line_canvas.coords(arrow, 300, 334, x, y)
-#         root.after(1000, update, i+1)
+def update(i):
+    if i<=AMOUNT_OF_SPEAKERS:
+        x = dictionary_arrow[i][0]
+        y = dictionary_arrow[i][1]
+        line_canvas.coords(arrow, 300, 334, x, y)
+        root.after(600, update, i+1)
+    else:
+        root.after(0, update, 1)
 
 
 
 arrow = line_canvas.create_line(300, 334, dictionary_arrow[5][0], dictionary_arrow[5][1], fill='red', width=10, arrow='last')
 
 
-# root.after(1000, update, 1)
+root.after(600, update, 1)
+
+# def pierwszy():
+#     print("Thread 1 started")
+#     input1 = sd.InputStream(samplerate=FS, channels=2, device=1, callback=audio_callback1)
+#     with input1:
+#         print(f"input1: {input1.active}")
+#
+# def drugi():
+#     print("Thread 2 started")
+#     input2 = sd.InputStream(samplerate=FS, channels=2, device=2, callback=audio_callback2)
+#     with input2:
+#         print(f"input2: {input2.active}")
+
+# input1 = sd.InputStream(samplerate=FS, channels=2, device=1, callback=audio_callback1)
+#
+# input2 = sd.InputStream(samplerate=FS, channels=2, device=2, callback=audio_callback2)
 
 
+# ani = FuncAnimation(fig, update_plot, init_func=plot_init, interval=ANIMATION_INTERVAL, blit=True, cache_frame_data=False)
+
+# thread1 = Thread(target=pierwszy)
+# thread2 = Thread(target=drugi)
+#
+# thread1.start()
+# thread2.start()
+# # plt.show()
+# print(f"Thread 1 alive: {thread1.is_alive()}")
+# print(f"Thread 2 alive: {thread2.is_alive()}")
+# root.mainloop()
+# thread1.join()
+# thread2.join()
 
 
-
-
-
-
-
-
-input = sd.InputStream(samplerate=FS, channels=2, device=1, callback=audio_callback)
-
-ani = FuncAnimation(fig, update_plot, init_func=plot_init, interval=ANIMATION_INTERVAL, blit=True, cache_frame_data=False)
-
-
-with input:
-    # plt.show()
-    root.mainloop()
-
+# with input1 and input2:
+#
+#     print(input1.is_active())
+#     print(input2.is_active())
+#     plt.show()
+root.mainloop()
 
--- psio/projekt/mikro/ys.py
+++ psio/projekt/mikro/ys.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import numpy as np
+from PIL import Image, ImageTk
 from pprint import pprint
 
 ARROW_LENGTH = 200
@@ -13,8 +14,8 @@ ANGLE = 180/(AMOUNT_OF_SPEAKERS-1)
 
 
 def coordinates_of_speakers(angle):
-    x = WIDTH / 2 - np.cos(np.radians(angle)) * (ARROW_LENGTH+50) - 3
-    y = HEIGHT - np.sin(np.radians(angle)) * (ARROW_LENGTH+50) - 85
+    x = WIDTH / 2 - np.cos(np.radians(angle)) * 250-27
+    y = HEIGHT - np.sin(np.radians(angle)) * 250 -110
     return x,y
 
 def coordinates_of_arrow(angle):
@@ -36,19 +37,33 @@ root.configure(background='#00c3e3')
 root.title("yś")
 root.geometry('600x400')
 
-line_canvas = tk.Canvas(root, width=620, height=400, background='#00c3e3')
+line_canvas = tk.Canvas(root,highlightthickness=0, width=620, height=400, background='#00c3e3')
 line_canvas.place(x=0, y=0)
-line_canvas.create_line(300,334,dictionary_arrow[6][0]+15, dictionary_arrow[6][1]+15,fill='red', width=10, arrow='last')
+line_canvas.create_line(300,334,dictionary_arrow[5][0]+15, dictionary_arrow[5][1]+15,fill='red', width=10, arrow='last')
 
 
 
+# for i in range(0,AMOUNT_OF_SPEAKERS):
+#     speakers_canvas = tk.Canvas(root, width=WIDTH_SPEAKER, height=HEIGHT_SPEAKER)
+#     speakers_canvas.configure(bg='green')
+#     angle = i*ANGLE
+#     x,y = coordinates_of_speakers(angle)
+#     speakers_canvas.place(x=x-15,y=y+15)
+angle_of_rotate = 0
 for i in range(0,AMOUNT_OF_SPEAKERS):
-    speakers_canvas = tk.Canvas(root, width=WIDTH_SPEAKER, height=HEIGHT_SPEAKER)
-    speakers_canvas.configure(bg='black')
-    angle = i*ANGLE
-    x,y = coordinates_of_speakers(angle)
-    speakers_canvas.place(x=x-15,y=y+15)
+    speakers_canvas = tk.Canvas(root,highlightthickness=0, width=70, height=70,background='#00c3e3')
+    speakers_canvas.pack()
+    original_image = Image.open("speaker.png")
+    resized_image = original_image.resize((90, 90))
 
+    rotated_image = resized_image.rotate(angle_of_rotate)
+    image = ImageTk.PhotoImage(rotated_image)
+    speakers_canvas.image = image
+    speakers_canvas.create_image(-3,-2,image=image,anchor='nw')
+    angle = i * ANGLE
+    x, y = coordinates_of_speakers(angle)
+    speakers_canvas.place(x=x - 15, y=y + 15)
 
+    angle_of_rotate -= ANGLE
 
 root.mainloop()
