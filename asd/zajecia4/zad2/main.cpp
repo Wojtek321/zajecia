a8dba1b47946a5a25b29ddd5990cd779e05611c5
Wojciech Domański <wojtekdxd@gmail.com>
2023-12-22 19:07:38 +0100

    dsf

--- asd/zajecia4/zad2/main.cpp
+++ asd/zajecia4/zad2/main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
-#include <iomanip>
+
 
 using namespace std;
-
-void johnson();
-void ford_bellman(int n);
-void dijkstry();
 
 struct el
 {
@@ -16,29 +11,26 @@ struct el
     int v, w;
 };
 
-el ** list;
-long long *d;
-int *p;
-el * pv;
-el * pz;
-int MAXINT = 2147483647;
+int MAXINT = 2147483647/2;
+ofstream output(R"(C:\Users\wojte\Desktop\zajecia\asd\zajecia4\zad2\Out0402.txt)");
 
-void ford_bellman(int n)
+void Johnson(el **list, int n);
+int* Ford_Bellman(el **list, int n, int s);
+int* Dijkstry(el **list, int n, int u);
+
+
+void Johnson(el **list, int n)
 {
     el ** list2 = new el * [n+1];
-    long long *d2;
-    int *p2;
-    d2 = new long long [n+1];
-    p2 = new int [n+1];
+    el *pv;
+    el *pz;
 
     for(int i=0; i<n+1; i++)
     {
-        d2[i] = MAXINT;
-        p2[i] = -1;
         list2[i] = NULL;
     }
 
-    for(int a=1; a<n+1; a++)
+    for(int a=1; a<=n; a++)
     {
         pv = new el;
         pv->v = a;
@@ -47,13 +39,14 @@ void ford_bellman(int n)
         list2[0] = pv;
     }
 
-    for(int i=0; i<n+0; i++)
+    for(int i=0; i<n; i++)
     {
         pv = list [ i ];
         while(pv)
         {
             pz = new el;
             pz->v = pv->v;
+            pz->v += 1;
             pz->w = pv->w;
             pz->next = list2[i+1];
             list2[i+1] = pz;
@@ -61,29 +54,179 @@ void ford_bellman(int n)
         }
     }
 
-//    for(int i = 0; i < n+1; i++ )
-//    {
-//        cout << "A [ " << i << " ] =";
-//        pv = list2 [ i ];
-//        while( pv )
-//        {
-////            cout <<setw ( 3 ) <<  pv->v;
-//            cout<<" wierzcholek: "<<pv->v<<", waga: "<<pv->w<<", next: "<<pv->next;
-//            pv = pv->next;
-//        }
-//        cout << endl;
-//    }
-    bool test;
-    d[0] = 0;
-    for(int i=1; i<n+1; i++)
-    {
 
+
+
+    int *h = Ford_Bellman(list2, n+1, 0);
+
+    for(int i=0; i<n+1; i++)
+    {
+        output<<h[i]<<" ";
+    }
+    output<<endl;
+
+
+
+    for(int i = 0; i < n+1; i++ )
+    {
+        output << "["<<i<<"] ";
+        pv = list2 [ i ];
+        while( pv )
+        {
+            pv->w = pv->w + h[i] - h[pv->v];
+            output<<pv->v<<"("<<pv->w<<") ";
+            pv = pv->next;
+        }
+        output << endl;
+    }
+
+
+
+    el ** list3 = new el * [n];
+    for(int i=0; i<n; i++)
+    {
+        list3[i] = NULL;
+    }
+
+    for(int i = 0; i < n; i++ )
+    {
+        pv = list2 [ i+1 ];
+        while( pv )
+        {
+            pz = new el;
+            pz->v = pv->v-1;
+            pz->w = pv->w;
+            pz->next = list3[i];
+            list3[i] = pz;
+            pv = pv->next;
+        }
+    }
+
+
+    for(int i=0; i<n; i++)
+    {
+        int *delta = Dijkstry(list3, n, i);
+        int *d = Ford_Bellman(list, n, i);
+
+
+        output<<"Delta^["<<i+1<<"][";
+        if(delta[0]>MAXINT-10000 || delta[0]<-MAXINT+10000)
+            output<<"-";
+        else
+            output<<delta[0];
+        for(int j=1; j<n-1; j++)
+        {
+            if(delta[j]>MAXINT-10000 || delta[j]<-MAXINT+10000)
+                output<<" -";
+            else
+                output<<" "<<delta[j];
+        }
+        if(delta[n-1]>MAXINT-10000 || delta[n-1]<-MAXINT+10000)
+            output<<" -";
+        else
+            output<<" "<<delta[n-1];
+
+
+
+        output<<"], D["<<i+1<<"][";
+        if(d[0]>MAXINT-10000 || d[0]<-MAXINT+10000)
+            output<<"-";
+        else
+            output<<d[0];
+        for(int j=1; j<n-1; j++)
+        {
+            if(d[j]>MAXINT-10000 || d[j]<-MAXINT+10000)
+                output<<" -";
+            else
+                output<<" "<<d[j];
+        }
+        if(d[n-1]>MAXINT-10000 || d[n-1]<-MAXINT+10000)
+            output<<" -]";
+        else
+            output<<" "<<d[n-1]<<"]";
+
+        output<<endl;
     }
 }
 
+int* Dijkstry(el **list, int n, int v)
+{
+    int *d = new int[n];
+    int *p = new int[n];
+    bool *QS = new bool[n];
+    el *pv;
+
+    for(int i=0; i<n; i++)
+    {
+        d[i] = MAXINT;
+        p[i] = -1;
+        QS[i] = false;
+    }
+
+    d[v] = 0;
+    int j, u;
+
+    for(int i = 0; i < n; i++ )
+    {
+        for( j = 0; QS [ j ]; j++ );
+
+        for( u = j++; j < n; j++ )
+            if( !QS [ j ] && ( d [ j ] < d [ u ] ) ) u = j;
+
+        QS [ u ] = true;
+
+        for( pv = list[ u ]; pv; pv = pv->next )
+            if( !QS [ pv->v ] && ( d [ pv->v ] > d [ u ] + pv->w ) )
+            {
+                d [ pv->v ] = d [ u ] + pv->w;
+                p [ pv->v ] = u;
+            }
+    }
+
+    return d;
+}
+
+int* Ford_Bellman(el **list, int n, int s)
+{
+    int *d = new int [n];
+    int *p = new int [n];
+    el *pv;
+
+    for(int i=0; i<n; i++)
+    {
+        d[i] = MAXINT;
+        p[i] = -1;
+    }
+
+    bool test;
+    d[s] = 0;
+    for(int i=1; i<n; i++)
+    {
+        test = true;
+        for(int x=0; x<n; x++)
+        {
+            for(pv=list[x]; pv; pv=pv->next)
+            {
+                if(d[pv->v] > d[x] + pv->w)
+                {
+                    test = false;
+                    d[pv->v] = d[x] + pv->w;
+                    p[pv->v] = x;
+                }
+            }
+        }
+        if(test)
+        {
+            return d;
+        }
+    }
+    return d;
+}
+
+
 int main()
 {
-    ifstream file("C:\\Users\\wojte\\Desktop\\zajecia\\asd\\zajecia4\\zad2\\In0402.txt");
+    ifstream file(R"(C:\Users\wojte\Desktop\zajecia\asd\zajecia4\zad2\In0402.txt)");
 
     string linia;
     int n;
@@ -92,17 +235,14 @@ int main()
     istringstream line(linia);
     line >> n;
 
-    list = new el * [n];
-    d = new long long [n];
-    p = new int [n];
+    el **list = new el * [n];
+    el *pv;
     int wierzcholek, waga;
 
 
     for(int i=0; i<n; i++)
     {
         list[i] = NULL;
-        p[i] = -1;
-        d[i] = MAXINT;
     }
 
 
@@ -114,7 +254,7 @@ int main()
         while (line >> wierzcholek >> waga)
         {
             pv = new el;
-            pv->v = wierzcholek;
+            pv->v = wierzcholek-1;
             pv->w = waga;
             pv->next = list[i];
             list[i] = pv;
@@ -124,21 +264,6 @@ int main()
     file.close();
 
 
-
-
-    for( i = 0; i < n; i++ )
-    {
-        cout << "A [ " << i << " ] =";
-        pv = list [ i ];
-        while( pv )
-        {
-//            cout <<setw ( 3 ) <<  pv->v;
-            cout<<" wierzcholek: "<<pv->v<<", waga: "<<pv->w<<", next: "<<pv->next;
-            pv = pv->next;
-        }
-        cout << endl;
-    }
-    cout<<endl;
-
-    ford_bellman(n);
+    Johnson(list, n);
+    output.close();
 }
